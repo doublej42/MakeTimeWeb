a0bbb08ec23efe38b64aaad4d979ce7174a90c36
Jeff Jacob <phansoft@gmail.com>
2023-03-04 11:30:42 -0800

    Add project files.

---
+++ MakeTimeWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MakeTimeWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MaketimePages/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MaketimePages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        //We need a place to store the name
+        [BindProperty]
+        public string MyName { get; set; }
+
+        public void OnGet()
+        {
+            
+        }
+
+        public void OnPost()
+        {
+            _logger.LogDebug(MyName);
+        }
+
+    }
+}
